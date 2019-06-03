3161d016c1f4f6b4c6936e0a5588a7af83f3335d
Faical Yannick Congo <yannick.congo@gmail.com>
2019-06-03 02:11:01 +0200

    setup change to avoid pip based dependencies.

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ def req_link(external_url):
 reqs_default = join(dirname(__file__), "requirements.txt")
 reqs_core = join(dirname(__file__), "requirements.core.txt")
 required = []
+pypied = []
 
 if exists(reqs_default):
     with open(reqs_default) as f:
@@ -18,7 +19,7 @@ if exists(reqs_default):
 
 if exists(reqs_core):
     with open(reqs_core) as f:
-        required += f.read().splitlines()
+        pypied += f.read().splitlines()
 
 with open(join(dirname(__file__), "README.rst")) as f:
     long_desc = f.read()
@@ -41,4 +42,7 @@ setup(
     include_package_data=True,
     install_requires=required,
     dependency_links=dep_links,
+    extras_require={
+        'pypi': pypied
+    },
 )
